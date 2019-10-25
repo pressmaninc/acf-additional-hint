c5f4a2fcd4867354a774f4936d9c688f52b30913
本間小春 <koharu_homma@pressman.ne.jp>
2019-10-25 11:58:48 +0900

    Change position of help button

--- js/main.js
+++ js/main.js
@@ -1,34 +1,16 @@
 jQuery(function ($) {
-    var parent = $('.hint-btn').parents('.acf-field');
-    var hintBtn = $('.hint-btn');
-    // positionでのボタンの移動(実装途中)
-    // $(window).load(function() {
-    //     var xpos = parent.innerWidth() - ( hintBtn.position().left + hintBtn.outerWidth() );
-    //     var ypos = parent.offset().top;
-    //     hintBtn.offset({top: ypos, left: xpos});
-    // });
+    // change position of hint icon & tooltip
+    // var parent = $('.tooltip1').parents('.acf-field');
+    // var fieldTitle = parent.find('.acf-label').find('label');
+    // fieldTitle.append($('.tooltip1'));
+
 
     $(window).load(function() {
-        // prependでのボタンの移動(実装途中)
-        // if ( ! $('.acf-field > .hint-btn') ) {
-        //     parent.prepend(hintBtn);
-        // }
-        // parent.prepend(hintBtn);
-
         // click event
         $('.hint-btn').on('click', function() {
             let $hintText = $(this).parent().find('.hint-text');
             var $acfField = $(this).parents('.acf-field');
-            // hint用テキストの移動(実装途中)
-            if ( $hintText.css('display') == 'block' ) {
-                $hintText.slideUp();
-                $hintText = $hintText.detach();
-            } else {
-                $hintText.slideDown();
-                $acfField.append($hintText);
-            }
-            // $acfField.append($hintText);
-            // $hintText.slideToggle();
+            $hintText.slideToggle();
     
         });
     });
